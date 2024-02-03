1a171fc51759ca1a6229f2d724db6559d6117a4e
Andrew <andrewtudoroi@gmail.com>
2024-02-04 00:38:18 +0100

    Simple uart transmitter draft design

---
+++ uart/uart.v
@@ -0,0 +1,99 @@
+module top(
+    input CLK_i,
+    output PMOD_2,
+    output D1,
+    output PMOD_4
+   
+    );
+
+    reg [7:0] txd = 8'h21;
+    reg tx_start = 1'b1;
+
+    uart_transmitter UART_TRANSMIT(
+        .clk(CLK_i),
+        .TxD_start(tx_start),
+        .TxD_data(txd),
+        .TxD(PMOD_4),
+        .TxD_busy(D1)
+    );
+
+endmodule
+
+
+module uart_transmitter (
+    input clk, 
+    input TxD_start,
+    input [7:0] TxD_data,
+    output TxD,
+    output TxD_busy
+);
+
+parameter ClkFrequency = 12000000;	// 25MHz
+parameter Baud = 115200;
+
+generate
+	if(ClkFrequency<Baud*8 && (ClkFrequency % Baud!=0)) ASSERTION_ERROR PARAMETER_OUT_OF_RANGE("Frequency incompatible with requested Baud rate");
+endgenerate
+
+////////////////////////////////
+`ifdef SIMULATION
+wire BitTick = 1'b1;  // output one bit per clock cycle
+`else
+wire BitTick;
+BaudTickGen #(ClkFrequency, Baud) tickgen(.clk(clk), .enable(TxD_busy), .tick(BitTick));
+`endif
+    
+reg [3:0] TxD_state = 0;
+wire TxD_ready = (TxD_state==0);
+assign TxD_busy = ~TxD_ready;
+
+reg [7:0] TxD_shift = 0;
+always @(posedge clk)
+begin
+	if(TxD_ready & TxD_start)
+		TxD_shift <= TxD_data;
+	else
+	if(TxD_state[3] & BitTick)
+		TxD_shift <= (TxD_shift >> 1);
+
+	case(TxD_state)
+		4'b0000: if(TxD_start) TxD_state <= 4'b0100;
+		4'b0100: if(BitTick) TxD_state <= 4'b1000;  // start bit
+		4'b1000: if(BitTick) TxD_state <= 4'b1001;  // bit 0
+		4'b1001: if(BitTick) TxD_state <= 4'b1010;  // bit 1
+		4'b1010: if(BitTick) TxD_state <= 4'b1011;  // bit 2
+		4'b1011: if(BitTick) TxD_state <= 4'b1100;  // bit 3
+		4'b1100: if(BitTick) TxD_state <= 4'b1101;  // bit 4
+		4'b1101: if(BitTick) TxD_state <= 4'b1110;  // bit 5
+		4'b1110: if(BitTick) TxD_state <= 4'b1111;  // bit 6
+		4'b1111: if(BitTick) TxD_state <= 4'b0010;  // bit 7
+		4'b0010: if(BitTick) TxD_state <= 4'b0011;  // stop1
+		4'b0011: if(BitTick) TxD_state <= 4'b0000;  // stop2
+		default: if(BitTick) TxD_state <= 4'b0000;
+	endcase
+end
+
+assign TxD = (TxD_state<4) | (TxD_state[3] & TxD_shift[0]);  // put together the start, data and stop bits
+
+endmodule
+
+
+////////////////////////////////////////////////////////
+module BaudTickGen(
+	input clk, enable,
+	output tick  // generate a tick at the specified baud rate * oversampling
+);
+parameter ClkFrequency = 12000000;
+parameter Baud = 115200;
+parameter Oversampling = 1;
+
+function integer log2(input integer v); begin log2=0; while(v>>log2) log2=log2+1; end endfunction
+localparam AccWidth = log2(ClkFrequency/Baud)+8;  // +/- 2% max timing error over a byte
+reg [AccWidth:0] Acc = 0;
+localparam ShiftLimiter = log2(Baud*Oversampling >> (31-AccWidth));  // this makes sure Inc calculation doesn't overflow
+localparam Inc = ((Baud*Oversampling << (AccWidth-ShiftLimiter))+(ClkFrequency>>(ShiftLimiter+1)))/(ClkFrequency>>ShiftLimiter);
+always @(posedge clk) if(enable) Acc <= Acc[AccWidth-1:0] + Inc[AccWidth:0]; else Acc <= Inc[AccWidth:0];
+assign tick = Acc[AccWidth];
+endmodule
+
+////////////////////////////////////////////////////////
