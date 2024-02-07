d973650498c69c6e9f1947f25404b4d55d8b7479
Andrew <andrewtudoroi@gmail.com>
2024-02-07 12:16:59 +0100

    Turn off glowing led's

--- button/button.v
+++ button/button.v
@@ -2,9 +2,15 @@ module top (
     input PMOD_4,
     input PMOD_3,
     output D1,
-    output D2
+    output D2,
+    output D3,
+    output D4, 
+    output D5
 );
     assign D1 = PMOD_4;
     assign D2 = PMOD_3;
+    assign D3 = 0;
+    assign D4 = 0;
+    assign D5 = 0;
 
 endmodule
