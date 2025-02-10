d40e17ddcff04a20a51adf30d76e20317b6fcfc1
atudoroi <atudoroi@yahoo.com>
2025-02-10 22:36:53 +0100

    Update tb

--- hcms_29xx/hcms_29xx_tb.v
+++ hcms_29xx/hcms_29xx_tb.v
@@ -1,10 +1,10 @@
-`timescale 1ns/1ps
+`timescale 1ns/10ps
 
 
 module top_tb();
 
 reg CLK_i = 0;
-
+reg [7:0] data;
 
 localparam  DURATION = 10000000;
 
@@ -14,16 +14,31 @@ always begin
     CLK_i = ~CLK_i;
 end
 
+reg load_data = 0; 
 
-hmcs_serial uut(
-    .CLK_i(CLK_i)
-
+hcms_serial uut(
+    .CLK_i(CLK_i),
+    .DATA_i(data),
+    .DATA_LOAD(load_data)
 
 );
 
 initial begin
+
+    #(2 * 41.67)
+    data = 8'h04;
+    load_data = 1'b0;     
+    // Clear read addr and enable  signal 
+    #(2 * 41.67)
+    data = 8'h32;
+    load_data = 1'b1;
+
+    #(2 * 41.67)
+    data = 8'h64;
+    load_data = 1'b1;  
+
      $dumpfile("hcms_29xx_tb.vcd");
-     $dumpvars(0, uut );
+     $dumpvars(0, top_tb );
 
      #(DURATION)
      $display("Finished!");
--- hcms_29xx/hcms_serial.v
+++ hcms_29xx/hcms_serial.v
@@ -1,7 +1,82 @@
+// Send cmd/data byte to hcms29 display 
 module hcms_serial (
-    input CLK_i
+    // Clock
+    input CLK_i,
+    // Data to be sent 
+    input [7:0] DATA_i,
+    // Cntrl 
+    input DATA_LOAD,
+    output reg TX_DONE,
+
+
+    // HCMS29XX connections  
+    output reg SER_DATA,
+    output RSEL,
+    output SER_CLK,
+    output nCE,
+    output nRESET
+
+
 );
     
+// Reset signal 
+reg  reset = 1'b0;
 
+// SM states 
+localparam  IDLE = 'd0,
+            SEND = 'd1,
+            DONE = 'd2; 
 
+// FSM Reg
+reg [1:0] state = IDLE;
+
+// Transmit logic ctr reg's 
+reg [2:0] tx_bit_index;
+reg [7:0] shiftReg;
+// Hardware ctrl 
+reg CE = 0;
+assign SER_CLK = (CE == 1'b1) ? CLK_i : 1'b1;
+
+always @(negedge CLK_i) begin
+    
+    if (reset) begin
+        state <= IDLE;
+    end 
+    else begin
+        case (state)
+            IDLE : begin 
+                if (DATA_LOAD == 1'b1) begin 
+                state <= SEND;
+                tx_bit_index <= 0;
+                shiftReg <= DATA_i;
+                end
+            end
+            SEND : begin 
+                // SET CE while transmitting 
+                SER_DATA <= shiftReg[7];
+                shiftReg <= {shiftReg[7:0], 1'b0};
+                TX_DONE <= 0;
+                CE <= 1; 
+
+                if (tx_bit_index < 7 )
+                    tx_bit_index <= tx_bit_index + 1;
+                else
+                    state <= DONE;
+
+            end
+            DONE : begin
+                // Transmit finalized set ce to 0
+                CE <= 0; 
+                TX_DONE <= 1;
+                // state <= IDLE;
+
+                if(DATA_LOAD == 1'b0)begin
+                    TX_DONE <= 1'b0; 
+                    state <= IDLE;
+                end
+            end
+            
+        endcase
+    end
+end
 endmodule
